e8bda85fdf89dd51eb6cb1c036d4aa5b178e8d4a
DedoDePescado03 <gaelloretomiranda@gmail.com>
2026-03-23 16:08:53 -0700

    explain POST flow and search time measurement

--- web/app.py
+++ web/app.py
@@ -6,16 +6,20 @@ app = Flask(__name__)
 
 engine = SearchEngine("data/corpus.json")
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
 
+    #Default values for when there is no active search
     results = []
     query = ""
     search_time = 0
 
     if request.method == "POST":
+        #Get the query entered by the user from the HTML form
         query = request.form["query"]
 
+        #Measure search time to display it in the UI.
         start = time.time()
         results = engine.search(query)
         search_time = time.time() - start
@@ -24,11 +28,16 @@ def index():
         "index.html",
         results=results,
         query=query,
-        total_docs = engine.indexer.N,
-        vocab_size = len(engine.indexer.index),
+        #Total number of documents indexed in the corpus
+        total_docs=engine.indexer.N,
+        #Number of unique terms in the index
+        vocab_size=len(engine.indexer.index),
+        #Time taken to execute the search (in seconds)
         search_time=search_time,
-        len_results = len(results)
+        #Number of results found for the current query
+        len_results=len(results)
     )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
